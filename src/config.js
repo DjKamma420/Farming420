36edc537255dffd3c3494110f632f3e704351030
Dj_Kamma420 <320271127+DjKamma420@users.noreply.github.com>
2026-09-15 23:29:53 +0200

    Add app version configuration

---
+++ src/config.js
@@ -0,0 +1,5 @@
+export const APP_VERSION = '0.3.0';
+export const DATA_SCHEMA_VERSION = 1;
+export const STORAGE_KEY = 'skyblock-farming-maxer-v1';
+export const BACKUP_FORMAT = 'farming420-backup';
+export const BACKUP_VERSION = 1;
