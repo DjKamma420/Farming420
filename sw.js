003f7aee38305337603a3a97a0754f077014a5a8
Dj_Kamma420 <320271127+DjKamma420@users.noreply.github.com>
2026-09-16 00:00:11 +0200

    Cache normalized profile model in app version 0.5.0

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'farming420-0.4.0';
+const VERSION = 'farming420-0.5.0';
 const APP_FILES = [
   './',
   './index.html',
@@ -13,6 +13,7 @@ const APP_FILES = [
   './src/migrations.js',
   './src/backup.js',
   './src/hypixel-import.js',
+  './src/profile-normalizer.js',
   './src/foundation.js',
   './src/foundation.css',
 ];
