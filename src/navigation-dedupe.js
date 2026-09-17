c5b91fa41f5038744ca6bbe48d5035edfb7e97ce
Dj_Kamma420 <320271127+DjKamma420@users.noreply.github.com>
2026-09-17 10:45:04 +0200

    Merge guide navigation into dashboard

--- src/navigation-dedupe.js
+++ src/navigation-dedupe.js
@@ -3,6 +3,7 @@ import { STORAGE_KEY } from './config.js';
 export const DUPLICATE_PAGE_TARGETS = Object.freeze({
   gear: 'setups',
   pets: 'setups',
+  guide: 'dashboard',
 });
 
 export function canonicalPage(page) {
