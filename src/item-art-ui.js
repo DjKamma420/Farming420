b2837d2ad0ac88dac6a0f8732648330a302c3f15
Dj_Kamma420 <320271127+DjKamma420@users.noreply.github.com>
2026-09-18 11:05:45 +0200

    Bust cached setup skull model module

--- src/item-art-ui.js
+++ src/item-art-ui.js
@@ -1,6 +1,6 @@
 import { STORAGE_KEY } from './config.js';
 import { itemAssetForSkyblockId, loadItemAssetManifest } from './item-assets.js';
-import { knownSkyblockHeadTexture, knownSkyblockRenderedIcon, skullTextureUrl } from './skull-art.js';
+import { knownSkyblockHeadTexture, knownSkyblockRenderedIcon, skullTextureUrl } from './skull-art.js?v=20260918-2';
 
 let manifest = null;
 let manifestRequested = false;
